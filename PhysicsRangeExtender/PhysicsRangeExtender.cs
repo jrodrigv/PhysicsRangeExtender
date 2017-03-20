1e87fed2302b3ba0aed8bad23b9e5c450ad805d9
Jesus Rodriguez <jesus.rodriguez.valencia@gmail.com>
2017-03-20 21:05:51 +0100

    Initial check in

---
+++ PhysicsRangeExtender/PhysicsRangeExtender.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+namespace PhysicsRangeExtender
+{
+    [KSPAddon(KSPAddon.Startup.FlightAndKSC, false)]
+    public class PhysicsRangeExtender : MonoBehaviour
+    {
+        private readonly VesselRanges.Situation _maxSituation = new VesselRanges.Situation(181000, 190000, 200000, 172900);
+
+        void Start()
+        {
+            FloatingOrigin.fetch.threshold = Mathf.Pow(200000 + 3500, 2);
+
+            GameEvents.onVesselSwitching.Add(ApplyPhysRange);
+            GameEvents.onVesselCreate.Add(ApplyPhysRange);
+            GameEvents.onVesselGoOnRails.Add(ApplyPhysRange);
+            GameEvents.onVesselGoOffRails.Add(ApplyPhysRange);
+            GameEvents.onVesselLoaded.Add(ApplyPhysRange);
+
+            ApplyPhysRange();
+
+        }
+
+        private void ApplyPhysRange(Vessel data0, Vessel data1)
+        {
+            ApplyPhysRange();
+        }
+
+        private void ApplyPhysRange(Vessel v)
+        {
+            ApplyPhysRange();
+        }
+
+        public void ApplyPhysRange()
+        {
+            try
+            {
+                foreach (var v in FlightGlobals.Vessels)
+                    v.vesselRanges = new VesselRanges(new VesselRanges
+                    {
+                        escaping = _maxSituation,
+                        flying = _maxSituation,
+                        landed = _maxSituation,
+                        orbit = _maxSituation,
+                        prelaunch = _maxSituation,
+                        splashed = _maxSituation,
+                        subOrbital = _maxSituation
+                    });
+
+            }
+            catch (Exception e)
+            {
+                Debug.Log("[PhysicsEnhancer]:Failed to Load Physics Distance -" + e);
+            }
+        }
+    }
+}
