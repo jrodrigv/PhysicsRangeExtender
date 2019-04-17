a61eda66ae041055f0e1652a72857ae2684cc104
Jesus Rodriguez <jesus.rodriguez.valencia@gmail.com>
2019-04-18 00:56:39 +0200

    removing not needed file

--- PhysicsRangeExtender/PQSData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PhysicsRangeExtender
-{
-    internal class PqsData
-    {
-        public double HorizonDistance { get; set; }
-        public double VisRadSeaLevelValue { get; set; }
-        public double CollapseSeaLevelValue { get; set; }
-    }
-}
